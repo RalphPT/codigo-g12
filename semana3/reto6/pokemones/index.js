ad99cf49b19e4b7e76ad8c58c9c2db198d5f55ee
Rafael F. Percca Trejo <perccatrejorafael@gmail.com>
2023-03-14 07:59:54 -0500

    Semana 6 api

---
+++ semana3/reto6/pokemones/index.js
@@ -0,0 +1,34 @@
+// const persona = {
+//     nombre: "Rafael",
+//     Apellido: "Percca",
+// }
+// persona.nombre
+// persona.apellido
+
+const container = document.querySelector(".container");
+
+async function getPokemons() {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+    const data = await response.json();
+
+    renderPokemons(data.results);
+    // data.results.forEach((pokemon) => {
+    //     container.innerHTML += `
+    //     <div class="card">
+    //     <h3>${pokemon.name}</h3>
+    //     <a href="${pokemon.url}" target="_blank">Ver detalle</a>
+    //     </div>`;
+    // });
+}
+function renderPokemons(pokemons) {
+    pokemons.forEach((pokemon) => {
+        container.innerHTML += `
+            <div class="card">
+            <h3>${pokemon.name}</h3>
+            <a href="${pokemon.url}" target="_blank">Ver detalle</a>
+            </div>`;
+    });
+}
+
+
+getPokemons();
--- semana4/reto7/index.js
+++ semana4/reto7/index.js
@@ -5,6 +5,8 @@ const image = document.querySelector("#image");
 //button submit
 const form = document.querySelector("form");
 
+const moviesContainer = document.querySelector(".movies");
+
 //array vacio
 const movies = [];
 
@@ -17,7 +19,35 @@ form.onsubmit = function (event) {
         description: description.value,
         image: image.value,
     });
-    movieName.value="";
-    description.value="";
-    image.value="";
+
+    // moviesContainer.innerHTML += `
+    // <div class="card mt-10">
+    // <h4>${movieName.value}</h4>
+    // <p>${description.value}</p>
+    // <img width="200" src="${image.value}" />
+    // </div>`
+
+    
+
+    movieName.value = "";
+    description.value = "";
+    image.value = "";
+
+    renderMovies();
 };
+
+function renderMovies() {
+    if (movies.length === 0) return;
+  
+    moviesContainer.innerHTML = "";
+  
+    movies.forEach((movie) => {
+      moviesContainer.innerHTML += `
+        <div class="card mt-10">
+          <h4>${movie.name}</h4>
+          <p>${movie.description}</p>
+          <img width="200" src="${movie.image}" />
+        </div>
+      `;
+    });
+  }
